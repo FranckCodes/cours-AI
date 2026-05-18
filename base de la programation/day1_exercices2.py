bb8c7d84732db509fc28ebff4a34f491ab57652b
franckcodes <choupole13@gmail.com>
2026-05-18 13:37:27 +0100

    manupilation des variables

---
+++ base de la programation/day1_exercices2.py
@@ -0,0 +1,21 @@
+#define variables of different types
+
+integer_var = 10
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (1, 2, 3)
+dict_var = {"name": "Alice", "role": "Engineer"}
+boolean_var = True
+
+
+
+#print and minuplate variables
+print("Integer variable:", integer_var)
+print("Float variable:", float_var)
+print("String variable:", string_var + " Bootcamp")
+list_var.append(6) #ajouter un element a la liste
+print("List variable:", list_var)
+print("Tuple variable:", tuple_var)
+print("Dictionary variable:", dict_var["name"]) #acceder a une valeur dans le dictionnaire
+print("Boolean variable:", boolean_var)
