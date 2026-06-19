26e17fba80f1e347b511eadd81d5cef29aca2f8e
franckcodes <choupole13@gmail.com>
2026-06-19 20:39:59 +0100

    day_2 les condition if, elif, else

---
+++ base de la programation/day_2/day2_exercies1.py
@@ -0,0 +1,9 @@
+#Example 1: Checking a condition 
+
+num = 0
+if num > 0:
+    print("The number is positive. ")
+elif num == 0: 
+    print("The number is zero. ")
+else:
+    print("The number is negative. ")
--- integer_and_float.py
+++ integer_and_float.py
@@ -5,3 +5,22 @@ height = 5.9
 #strings ou chaines de caractères 
 name = "Alice"
 
+greeting = "hello," + name
+
+
+#List and tuples
+fruits = ["apple", "banana", "cherry"]
+numbers = (1, 2, 3, 4, 5)
+
+coordinates = (10.5, 20.3)
+
+#Dictionnaires
+person = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+#booleans
+is_student = True
+is_employed = False
