981ba9e612c33c1d410d7c982eb45a1a587d582b
franckcodes <choupole13@gmail.com>
2026-06-20 17:47:44 +0100

    module utilisation

---
+++ base de la programation/day_3/exercices2.py
@@ -0,0 +1,10 @@
+import math_operations as mo
+
+
+num1 = 10
+num2 = 5
+
+print("Addition:", mo.add(num1, num2))
+print("Subtraction:", mo.substraction(num1, num2))
+print("Multiplication:", mo.multiply(num1, num2))
+print("Division:", mo.divide(num1, num2))
