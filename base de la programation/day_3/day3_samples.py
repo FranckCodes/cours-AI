734d77ec3054a2ee6dfeb55ce783912a95ffa078
franckcodes <choupole13@gmail.com>
2026-06-20 17:12:33 +0100

    utilisation de alias

--- base de la programation/day_3/day3_samples.py
+++ base de la programation/day_3/day3_samples.py
@@ -4,8 +4,14 @@
 
 # print (math.sqrt(16))
 
-from math import sqrt
+# from math import sqrt
 
-number_to_sqrt = int(input("Enter a number to find its square root: "))
+# number_to_sqrt = int(input("Enter a number to find its square root: "))
 
-print("The square root of", number_to_sqrt, "is", sqrt(number_to_sqrt))
+# print("The square root of", number_to_sqrt, "is", sqrt(number_to_sqrt))
+
+
+#using alias 
+
+import math as m
+print(m.sqrt(16))
