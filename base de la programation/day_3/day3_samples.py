476c3df0e2d2bffa6fea8f6a48bb21eba87fe286
franckcodes <choupole13@gmail.com>
2026-06-20 16:57:12 +0100

    les modules

--- base de la programation/day_3/day3_samples.py
+++ base de la programation/day_3/day3_samples.py
@@ -1,5 +1,11 @@
 #les modules 
 
-import math 
+# import math 
 
-print (math.sqrt(16))
+# print (math.sqrt(16))
+
+from math import sqrt
+
+number_to_sqrt = int(input("Enter a number to find its square root: "))
+
+print("The square root of", number_to_sqrt, "is", sqrt(number_to_sqrt))
