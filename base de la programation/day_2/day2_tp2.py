66e4e507c5e12148fd09d74e33cbd95166ebebad
franckcodes <choupole13@gmail.com>
2026-06-20 16:35:37 +0100

    day 2 finished

---
+++ base de la programation/day_2/day2_tp2.py
@@ -0,0 +1,7 @@
+# mon tp number 2 
+
+find_the_oldest_numbers = [25, 30, 35, 40, 45]
+
+for the_oldest_number in find_the_oldest_numbers:
+    if the_oldest_number == max(find_the_oldest_numbers):
+        print("The oldest number is:", the_oldest_number)
